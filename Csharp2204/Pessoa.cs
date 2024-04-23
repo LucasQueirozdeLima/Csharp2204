445f95d23fc5595a18fec4b829f02dceedd1d916
lucas.lima <lucas.lima716@etec.sp.gov.br>
2024-04-22 22:21:09 -0300

    1

---
+++ Csharp2204/Pessoa.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp2204
+{
+    class Pessoa
+    {
+        /*public int participantes;
+        public void cadastrar() 
+        {
+            for(int i = 0; i < participantes - 1; i++)
+            {
+                nn ligar para isso
+            }
+        }*/
+
+        private string nome;
+        private double salario;
+
+        public double Salario { //propriedade
+            get
+            {
+                return this.salario;
+            }
+            set 
+            {
+                if (this.salario > value)
+                {
+                    Console.WriteLine("Salario atual menor que o informado");
+                }
+                else
+                {
+                    this.salario = value;
+                    Console.WriteLine("Salario atualizado");
+                }
+            } 
+        }
+        
+
+        public Pessoa(string nome, double salario) // Metodo contrutor
+        {
+            this.nome = nome;
+            this.salario = 1000;
+        }
+
+        public string Nome { //propriedade
+            get
+            { 
+                return this.nome.ToUpper(); 
+            }
+
+            set 
+            {
+                this.nome = value;
+            }
+        }
+        public Pessoa() { }
+        public string Email { get; set; } //propriedade
+    }
+}
